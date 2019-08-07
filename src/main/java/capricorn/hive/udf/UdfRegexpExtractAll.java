b0eed711de3d0a19799cb7131174e36470cc2bf0
qbb <53695741+capricornQBB@users.noreply.github.com>
2019-08-07 16:04:28 +0800

    Update UdfRegexpExtractAll.java

--- src/main/java/capricorn/hive/udf/UdfRegexpExtractAll.java
+++ src/main/java/capricorn/hive/udf/UdfRegexpExtractAll.java
@@ -3,62 +3,55 @@ package capricorn.hive.udf;
 import org.apache.hadoop.hive.ql.exec.Description;
 import org.apache.hadoop.hive.ql.exec.UDF;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.regex.MatchResult;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
  * @Description :
  * CREATE TEMPORARY FUNCTION regexp_extract_all AS 'capricorn.hive.udf.UdfRegexpExtractAll';
- * SELECT regexp_extract_all('15913101234，abcd，421974511','^[0-9]*$','\t','@qq.com') from users limit 1;
+ * SELECT regexp_extract_all(name,'^[0-9]*$','\t') from users limit 1;
  * @Author : Capricorn.QBB
- * @Date : 2019-08-06
+ * @Date : 2019-08-07
  * @Version : 1.0
  */
 @Description(name = "regexp_extract_all",
-        value = "_FUNC_(haystack, pattern, delimiter) - Find all the instances of pattern in haystack and return a string.")
+        value = "_FUNC_(haystack, pattern, delimiter) - Find all the instances of pattern in haystack")
 public class UdfRegexpExtractAll extends UDF {
+
     private String lastRegex = null;
     private Pattern pattern = null;
 
-    public String evaluate(String input, String regex, String delimiter, String suffix) {
-        if (input == null || regex == null || "".equals(regex.trim())) {
+    public List<String> evaluate(String s, String regex, Integer extractIndex, String appendStr) {
+        if (s == null || regex == null || extractIndex == null) {
             return null;
         }
-
         if (!regex.equals(lastRegex)) {
             lastRegex = regex;
             pattern = Pattern.compile(regex, Pattern.MULTILINE);
         }
-
-        StringBuffer sb = new StringBuffer();
-
-        Matcher matcher = pattern.matcher(input);
+        LinkedList<String> result = new LinkedList<>();
+        Matcher matcher = pattern.matcher(s);
         while (matcher.find()) {
-            if (!"".equals(matcher.group(0))) {
-                if (suffix != null) {
-                    sb.append(delimiter).append(matcher.group(0)).append(suffix);
+            MatchResult mr = matcher.toMatchResult();
+            if (!"".equals(matcher.group(extractIndex))) {
+                if (appendStr != null) {
+                    result.add(mr.group(extractIndex) + appendStr);
                 } else {
-                    sb.append(delimiter).append(matcher.group(0));
+                    result.add(mr.group(extractIndex));
                 }
             }
         }
-
-        return sb.toString().replaceFirst(delimiter, "");
+        return result;
     }
 
-    public String evaluate(String s, String regex, String suffix) {
-        return this.evaluate(s, regex, ",", suffix);
+    public List<String> evaluate(String s, String regex) {
+        return this.evaluate(s, regex, 0, null);
     }
 
-    public String evaluate(String s, String regex) {
-        return this.evaluate(s, regex, ",", null);
+    public List<String> evaluate(String s, String regex, String appendStr) {
+        return this.evaluate(s, regex, 0, appendStr);
     }
-
-//	public static void main(String[] args) {
-//		UdfRegexpExtractAll u = new UdfRegexpExtractAll();
-//		String rs = u.evaluate("15913102833，111，421974539", "[1-9][0-9]{4,}", "@qq.com");
-//		System.out.println(rs);
-//		rs = u.evaluate("12123asldjfalkhf123zfhlsdk15913102833", "(?<!\\d)(?:(?:1[358]\\d{9})|(?:861[358]\\d{9}))(?!\\d)", "\t");
-//		System.out.println(rs);
-//	}
 }
