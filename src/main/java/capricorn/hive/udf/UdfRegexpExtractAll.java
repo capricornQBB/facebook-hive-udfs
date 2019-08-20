72ce3f314c0ecdbdbd8a88e58dd193b4244cfa01
capricornQbb <capricornqbb@foxmail.com>
2019-08-21 01:49:53 +0800

    add a udf

--- src/main/java/capricorn/hive/udf/UdfRegexpExtractAll.java
+++ src/main/java/capricorn/hive/udf/UdfRegexpExtractAll.java
@@ -18,40 +18,40 @@ import java.util.regex.Pattern;
  * @Version : 1.0
  */
 @Description(name = "regexp_extract_all",
-        value = "_FUNC_(haystack, pattern, delimiter) - Find all the instances of pattern in haystack")
+		value = "_FUNC_(haystack, pattern, delimiter) - Find all the instances of pattern in haystack")
 public class UdfRegexpExtractAll extends UDF {
 
-    private String lastRegex = null;
-    private Pattern pattern = null;
+	private String lastRegex = null;
+	private Pattern pattern = null;
 
-    public List<String> evaluate(String s, String regex, Integer extractIndex, String appendStr) {
-        if (s == null || regex == null || extractIndex == null) {
-            return null;
-        }
-        if (!regex.equals(lastRegex)) {
-            lastRegex = regex;
-            pattern = Pattern.compile(regex, Pattern.MULTILINE);
-        }
-        LinkedList<String> result = new LinkedList<>();
-        Matcher matcher = pattern.matcher(s);
-        while (matcher.find()) {
-            MatchResult mr = matcher.toMatchResult();
-            if (!"".equals(matcher.group(extractIndex))) {
-                if (appendStr != null) {
-                    result.add(mr.group(extractIndex) + appendStr);
-                } else {
-                    result.add(mr.group(extractIndex));
-                }
-            }
-        }
-        return result;
-    }
+	public List<String> evaluate(String s, String regex, Integer extractIndex, String appendStr) {
+		if (s == null || regex == null || extractIndex == null) {
+			return null;
+		}
+		if (!regex.equals(lastRegex)) {
+			lastRegex = regex;
+			pattern = Pattern.compile(regex, Pattern.MULTILINE);
+		}
+		LinkedList<String> result = new LinkedList<>();
+		Matcher matcher = pattern.matcher(s);
+		while (matcher.find()) {
+			MatchResult mr = matcher.toMatchResult();
+			if (!"".equals(matcher.group(extractIndex))) {
+				if (appendStr != null) {
+					result.add(mr.group(extractIndex) + appendStr);
+				} else {
+					result.add(mr.group(extractIndex));
+				}
+			}
+		}
+		return result;
+	}
 
-    public List<String> evaluate(String s, String regex) {
-        return this.evaluate(s, regex, 0, null);
-    }
+	public List<String> evaluate(String s, String regex) {
+		return this.evaluate(s, regex, 0, null);
+	}
 
-    public List<String> evaluate(String s, String regex, String appendStr) {
-        return this.evaluate(s, regex, 0, appendStr);
-    }
+	public List<String> evaluate(String s, String regex, String appendStr) {
+		return this.evaluate(s, regex, 0, appendStr);
+	}
 }
